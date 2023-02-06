d0ca6428d330263743068564d610aa946558acfb
Bastien Tran <bastien.tran@comet.team>
2023-02-06 11:27:08 +0100

    1_await_or_wait.test

--- 1_await_or_wait.test.ts
+++ 1_await_or_wait.test.ts
@@ -1,15 +1,11 @@
-
 // Your code below (Hint: setTimeout)
 // Should return a promise that will be rejected only after the given delay expressed in milliseconds
-const waitForDelay = (delayInMs: number) => Promise.reject('TODO');
-
-
+const waitForDelay = (delayInMs: number) =>
+  new Promise((resolve) => setTimeout(resolve, delayInMs));
 
 //////////////////////////////////////////////////
 
 describe("Wait for a delay", () => {
-
-  
   const advanceTimersByTime = (timeInMs: number) => {
     return new Promise((resolve) =>
       setImmediate(() => {
@@ -25,14 +21,12 @@ describe("Wait for a delay", () => {
     const task = async () => {
       await waitForDelay(30_000);
       jobDone = true;
-    }
+    };
     // when
     task();
     // then
     expect(jobDone).toBe(false);
     await advanceTimersByTime(30_000);
     expect(jobDone).toBe(true);
-    
   });
-
 });
